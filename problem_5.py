9913a51bf1d7bd355b76442d42b6999713c57211
Oliver Knowles <oliverknowles1@gmail.com>
2016-06-22 23:23:50 +0100

    Added working solution for problem 5

--- problem_5.py
+++ problem_5.py
@@ -1,14 +1,31 @@
-def divisible_by_range(n, max_range):
-    for i in range(1,max_range):
+def divisible_by_set(n, nums):
+    for i in nums:
         if ((n % i) != 0):
             return False
     return True
 
-def smallest_multiple(max_range):
+# find the unique factors in a given range, e.g. for upper_limit = 10
+# the unique factors would be 6,7,8,9,10
+# that is, if a number is divisible by 10 then it is also divisible by 5 so no need to check that
+def unique_factors(upper_limit):
+    factors = range(2, upper_limit + 1)
 
+    for i in range(1, upper_limit):
+        j = 2
+        while (i * j <= upper_limit):
+            if i * j in factors:
+                if i in factors:
+                    factors.remove(i)
+            j += 1
+    return factors
+
+print(unique_factors(20))
+
+def smallest_multiple(max_range):
+    facs = unique_factors(max_range)
     n = 2500
-    while (n < 100000000):
-        if (divisible_by_range(n, max_range)):
+    while (n < 1000000000):
+        if (divisible_by_set(n, facs)):
             return n
         n += 2
     return -1
