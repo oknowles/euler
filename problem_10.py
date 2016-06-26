b433b831e8dab3b01e63f65e75841d21ed35db0a
Oliver Knowles <oliverknowles1@gmail.com>
2016-06-26 22:41:56 +0100

    Implemented simple prime sieve to increase efficiency for problems 10 and 7

--- problem_10.py
+++ problem_10.py
@@ -1,34 +1,9 @@
-def is_prime(n):
-    if (n in [2, 3]):
-        return True
+def find_primes(n):
+    prime_flags = [True] * n
 
-    prime_factors = []
-    while (n % 2 == 0 and len(prime_factors) < 3):
-        if (len(prime_factors) == 1):
-            return False
-        prime_factors.append(2)
-        n //= 2
+    for p in range(3, int(n**0.5)+1, 2):
+        if prime_flags[p]:
+            prime_flags[2*p::p] = [False] * ((n-p-1)/p)
+    return [2] + [i for i in range(3, n, 2) if prime_flags[i]]
 
-    p = 3
-    while (n > 1 and len(prime_factors) < 3):
-        if (n % p == 0):
-            if (len(prime_factors) == 1):
-                return False
-            prime_factors.append(p)
-            n //= p
-        else:
-            p += 2
-
-    return len(prime_factors) == 1
-
-def sum_primes(upper_limit):
-    if upper_limit == 2:
-        return 2
-    total = 5
-    for p in range(5, upper_limit, 2):
-        if is_prime(p):
-            total += p
-    return total
-
-
-print(sum_primes(2000000))
+print(sum(find_primes(2000000)))
--- problem_7.py
+++ problem_7.py
@@ -1,33 +1,10 @@
-def is_prime(n):
-    if (n in [2, 3]):
-        return True
+def find_primes(n):
+    prime_flags = [True] * n
 
-    prime_factors = []
-    while (n % 2 == 0 and len(prime_factors) < 3):
-        prime_factors.append(2)
-        n //= 2
+    for p in range(3, int(n**0.5)+1, 2):
+        if prime_flags[p]:
+            prime_flags[2*p::p] = [False] * ((n-p-1)/p)
+    return [2] + [i for i in range(3, n, 2) if prime_flags[i]]
 
-    p = 3
-    while (n > 1 and len(prime_factors) < 3):
-        if (n % p == 0):
-            prime_factors.append(p)
-            n //= p
-        else:
-            p += 2
-
-    return len(prime_factors) == 1
-
-# returns the nth prime assuming n > 1
-def nth_prime(n):
-    num_primes = 2
-    cur_p = 3
-
-    while (num_primes < n):
-        cur_p += 2
-        while (!is_prime(cur_p)):
-            cur_p += 2
-        num_primes += 1
-
-    return cur_p
-
-print(nth_prime(10001))
+primes = find_primes(10001*50)
+print(primes[10001 - 1])
