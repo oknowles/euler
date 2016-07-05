17fbecdb980c0b360ed65d17cf64b26042fc130f
Oliver <oliver.knowles@smartology.net>
2016-07-05 13:33:08 +0100

    Added solution for problem 17 - Script should be cleaned up

--- problem_17.py
+++ problem_17.py
@@ -1,4 +1,15 @@
-num_map = {1:'one', 2:'two', 3:'three', 4:'four', 5:'five', 6:'six', 7:'seven', 8:'eight', 9:'nine', 11:'eleven', 12:'twelve', 13:'thirteen', 14:'fourteen', 15:'fifteen', 16:'sixteen', 17:'seventeen', 18:'eightenn', 19:'nineteen', 20:'twenty', 30:'thirty', 40:'forty', 50:'fifty', 60:'sixty', 70:'seventy', 80:'eighty', 90:'ninety'}
+num_map = {1:'one', 2:'two', 3:'three', 4:'four', 5:'five', 6:'six', 7:'seven', 8:'eight', 9:'nine', 10:'ten', 11:'eleven', 12:'twelve', 13:'thirteen', 14:'fourteen', 15:'fifteen', 16:'sixteen', 17:'seventeen', 18:'eightenn', 19:'nineteen', 20:'twenty', 30:'thirty', 40:'forty', 50:'fifty', 60:'sixty', 70:'seventy', 80:'eighty', 90:'ninety'}
+
+# if going from thousands to hundreds then put comma
+# if going from hundreds to tens or units then add 'and'
+# i.e for a 4 digit number, and and will only go in the 3 digit number (if applicable)
+
+# digits = 1 2 3 4, written_form = 'one thousand, two hundred and thirty four'
+#
+# cur_digit = 1, cur_size = 4 => 'one thousand'
+# cur_digit = 2, cur_size = 3 => '2 hundred and'
+# cur_digit = 3, cur_size = 2 => 'thirty'
+# cur_digit = 4, cur_size = 1 => 'four'
 
 def digit_list(n):
     digits = []
@@ -14,12 +25,37 @@ def get_written_form(n):
 
     written_form = ''
     digits = digit_list(n)
-    total_size = len(digits)
-    for i in range(total_size):
-        num_zeros = total_size - i
-        if num_zeros = 3:
-            written_form += str(digits[i]) + 'thousand'
 
-word_count = 0
-for i in range(1,10001):
-    word_count += get_written_form(i)
+    num_digits = len(digits)
+    for i in range(num_digits):
+        cur_digit = digits[i]
+        num_zeros = num_digits - (i + 1)
+
+        if (num_zeros == 3):
+            written_form += str(num_map[cur_digit]) + ' thousand, '
+        elif (num_zeros == 2) and (cur_digit != 0):
+                written_form += num_map[cur_digit] + ' hundred '
+        elif (num_zeros == 1):
+            if (num_digits > 2):
+                written_form += 'and '
+
+            tens_num = int(str(cur_digit) + str(digits[i+1]))
+            if (tens_num in num_map):
+                written_form += num_map[tens_num] + ' '
+                return written_form
+            elif (cur_digit != 0):
+                written_form += num_map[cur_digit*10] + ' '
+        elif (num_zeros == 0) and (cur_digit != 0):
+            written_form += num_map[cur_digit]
+
+    if (written_form.endswith(' and ')):
+        written_form = written_form.replace(' and ', '')
+
+    return written_form.strip(',')
+
+countable_words = ''
+for i in range(1,1001):
+    print(get_written_form(i))
+    countable_words += get_written_form(i).replace(' ','')
+
+print(len(countable_words))
